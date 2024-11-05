2872e7f3c99bb32ae6dfc5b5a557af8dee989188
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-11-05 13:58:52 -0500

    fix: Change package.json due RedHat Dependency mistake

--- backend/Song/handler/createSong.js
+++ backend/Song/handler/createSong.js
@@ -5,40 +5,16 @@ const { DynamoDB } = AWS;
 const dynamodb = new DynamoDB.DocumentClient();
 const TABLE_NAME = process.env.TABLE_NAME;
 
-async function getNextSongId() {
-  const params = {
-    TableName: TABLE_NAME,
-    Key: { pk: "songCounter" },
-    UpdateExpression: "ADD #cnt :val",
-    ExpressionAttributeNames: { "#cnt": "count" },
-    ExpressionAttributeValues: { ":val": 1 },
-    ReturnValues: "UPDATED_NEW",
-  };
-
-  console.log(params);
-
-  const result = await dynamodb.update(params).promise();
-  console.log(result);
-  return result.Attributes.count;
-}
-
 export async function handler(event) {
   const song =
     typeof event.body === "string" ? JSON.parse(event.body) : event.body;
 
-    console.log(song);
+  const params = {
+    TableName: TABLE_NAME,
+    Item: song,
+  };
 
   try {
-    const songId = await getNextSongId();
-    song.songId = songId;
-    console.log(song);
-    console.log(songId);
-
-    const params = {
-      TableName: TABLE_NAME,
-      Item: song,
-    };
-
     await dynamodb.put(params).promise();
     return {
       statusCode: 201,
