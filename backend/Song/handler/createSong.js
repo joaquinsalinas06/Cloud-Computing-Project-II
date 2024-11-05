a3ca74665ac660f787902e8bb53ddd7b9f757316
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-11-05 14:18:24 -0500

    fix: Changed Id obtention from atomic counter to max id

--- backend/Song/handler/createSong.js
+++ backend/Song/handler/createSong.js
@@ -9,9 +9,42 @@ export async function handler(event) {
   const song =
     typeof event.body === "string" ? JSON.parse(event.body) : event.body;
 
+  const providerId = song.providerId;
+
+  let highestSongId = 0;
+  try {
+    const response = await dynamodb.query({
+      TableName: TABLE_NAME,
+      KeyConditionExpression: "providerId = :providerId",
+      ExpressionAttributeValues: {
+        ":providerId": providerId,
+      },
+      ScanIndexForward: false, 
+      Limit: 1, 
+    }).promise();
+
+    if (response.Items.length > 0) {
+      highestSongId = response.Items[0].songId;
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: {
+        message: "Error al consultar el songId más alto",
+        error: error.message,
+      },
+    };
+  }
+
+  song.songId = highestSongId + 1;
+
   const params = {
     TableName: TABLE_NAME,
     Item: song,
+    ConditionExpression: "attribute_not_exists(providerId) AND attribute_not_exists(songId)",
   };
 
   try {
