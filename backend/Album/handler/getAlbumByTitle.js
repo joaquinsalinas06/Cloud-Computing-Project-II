fa40b98118f4c5aaaa0d35924f4cbd3e535c16d7
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-11-06 10:54:26 -0500

    feat: Add getAlbumById and with pagination implementations

---
+++ backend/Album/handler/getAlbumByTitle.js
@@ -0,0 +1,46 @@
+import "dotenv/config";
+import AWS from "aws-sdk";
+
+const { DynamoDB } = AWS;
+const dynamodb = new DynamoDB.DocumentClient();
+const TABLE_NAME = process.env.TABLE_NAME;
+const INDEX_NAME = process.env.INDEX_NAME;
+
+export async function handler(event) {
+  const title = event.query?.title;
+
+  if (!title) {
+    return {
+      statusCode: 400,
+      headers: { "Content-Type": "application/json" },
+      body: { message: "Falta el parámetro: title" },
+    };
+  }
+
+  const params = {
+    TableName: TABLE_NAME,
+    IndexName: INDEX_NAME,
+    KeyConditionExpression: "title = :title",
+    ExpressionAttributeValues: {
+      ":title": title,
+    },
+  };
+
+  try {
+    const data = await dynamodb.query(params).promise();
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: data.Items,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "application/json" },
+      body: {
+        message: "Error al buscar la canción por título",
+        error: error.message,
+      },
+    };
+  }
+}
