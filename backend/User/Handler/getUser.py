2342aff2eb122edf451d03896d3ec98f776c757b
zaamir5895 <zamir.lizardo@utec.edu.pe>
2024-11-06 17:01:06 -0500

    priorizing query over scan

--- backend/User/Handler/getUser.py
+++ backend/User/Handler/getUser.py
@@ -1,25 +1,25 @@
 import boto3
 import os
 
-import boto3.dynamodb
-
 def lambda_handler(event, context):
     provider_id = event['pathParameters']['provider_id']
     user_id = event['pathParameters']['user_id']
+    
     dynamodb = boto3.resource('dynamodb')
-    dynamodb = boto3.dynamodb.resource('dynamodb') 
     user_table_name = os.getenv('TABLE_NAME_e')
     user_table = dynamodb.Table(user_table_name)
-    response = user_table.get_item(
-        key = {
-            'provider_id': provider_id,
-            'user_id': user_id
-        }
+    
+    response = user_table.query(
+        KeyConditionExpression=boto3.dynamodb.conditions.Key('provider_id').eq(provider_id) & boto3.dynamodb.conditions.Key('user_id').eq(user_id)
     )
-    if 'Item' not in response:
+    
+    if 'Items' not in response or len(response['Items']) == 0:
         return {
-            'statusCode': 403,
-            'body': 'User doesn\'t exist'
+            'statusCode': 404,
+            'body': 'User not found'
         }
-    else:
-        user = response['Item']
+    
+    return {
+        'statusCode': 200,
+        'body': response['Items'][0]
+    }
