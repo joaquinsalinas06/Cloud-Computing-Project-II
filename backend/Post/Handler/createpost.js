3ff04af818ff887af2392c05132388465ef75e38
zaamir5895 <zamir.lizardo@utec.edu.pe>
2024-11-09 21:54:23 -0500

    f

--- backend/Post/Handler/createpost.js
+++ backend/Post/Handler/createpost.js
@@ -3,7 +3,8 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const { v4: uuidv4 } = require("uuid");
 
 module.exports.handler = async function (event) {
-  const { provider_id, user_id, song_id, playlist_id, titulo, descripcion } = JSON.parse(event.body);
+  const body = typeof event.body === "string" ? JSON.parse(event.body) : event.body;
+  const { provider_id, user_id, song_id, playlist_id, titulo, descripcion } = body;
   const post_id = uuidv4();
 
   const params = {
