cc05b67c5bed2aff862a857871a28649ffdd80bd
zaamir5895 <zamir.lizardo@utec.edu.pe>
2024-11-10 00:31:43 -0500

    ficing

--- backend/Post/Handler/createpost.js
+++ backend/Post/Handler/createpost.js
@@ -40,6 +40,17 @@ module.exports.handler = async function (event) {
       body: { error: "Authorization check failed", details: error.message }
     };
   }
+  function getFormattedDate() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Meses van de 0 a 11, así que sumamos 1
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+  
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
 
   const post_id = uuidv4();
   const params = {
@@ -52,7 +63,7 @@ module.exports.handler = async function (event) {
       playlist_id,
       titulo,
       descripcion,
-      created_at: new Date().toISOString()
+      created_at: getFormattedDate() 
     }
   };
 
