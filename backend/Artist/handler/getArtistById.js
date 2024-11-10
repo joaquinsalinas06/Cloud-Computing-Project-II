4415d79b2539f4ce9aa905237c335201ea6da3ed
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-11-10 00:13:44 -0500

    fix: Change get ArtistById

--- backend/Artist/handler/getArtistById.js
+++ backend/Artist/handler/getArtistById.js
@@ -5,36 +5,50 @@ const dynamodb = new DynamoDB.DocumentClient();
 const TABLE_NAME = process.env.TABLE_NAME;
 
 export async function handler(event) {
-	const { provider_id } = JSON.parse(event.body);
-  const artistId = event.pathParameters.artistId;
+  const provider_id = event.path?.provider_id;
+  const artist_id = event.path?.artist_id;
+
+  if (!provider_id || !artist_id) {
+    return {
+      statusCode: 400,
+      headers: { "Content-Type": "application/json" },
+      body: { message: "The parameters: provider_id or artist_id are missing" },
+    };
+  }
 
   const params = {
     TableName: TABLE_NAME,
     Key: {
       provider_id,
-      artistId,
+      artist_id: parseInt(artist_id, 10),
     },
   };
 
   try {
     const data = await dynamodb.get(params).promise();
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data.Item),
-    };
+    if (data.Item) {
+      return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: data.Item,
+      };
+    } else {
+      return {
+        statusCode: 404,
+        headers: { "Content-Type": "application/json" },
+        body: { message: "Artist not found" },
+      };
+    }
   } catch (error) {
     return {
       statusCode: 500,
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        message: "Error al obtener el artista",
+      body: {
+        message: "An error occurred while searching for the artist",
         error: error.message,
-      }),
+      },
     };
   }
 }
