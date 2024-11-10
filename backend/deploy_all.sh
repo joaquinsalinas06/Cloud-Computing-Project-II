cced80240af8cec0a1c136766af4bb75c9eb6f9e
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-11-10 14:39:55 -0500

    fix: Change baseURL answer

--- backend/deploy_all.sh
+++ backend/deploy_all.sh
@@ -4,37 +4,32 @@ RESULTS_FILE="deployment_summary.txt"
 
 > $RESULTS_FILE
 
-BASE_URLS=()
+declare -A BASE_URLS
 
 for dir in */; do
     if [ -f "$dir/serverless.yml" ]; then
         echo "Desplegando en la carpeta: $dir"
         cd "$dir" || exit
-
         DEPLOY_OUTPUT=$(sls deploy 2>&1)
-
         BASE_URL=$(echo "$DEPLOY_OUTPUT" | grep -Eo 'https?://[a-zA-Z0-9\-]+\.execute-api\.[a-zA-Z0-9\-]+\.amazonaws\.com/[a-zA-Z0-9]+')
-
         if [ -n "$BASE_URL" ]; then
-            BASE_URLS+=("$BASE_URL")
+            BASE_URLS["$BASE_URL"]="$dir"
         fi
-
         echo "Resultados de despliegue para $dir:" >> "../$RESULTS_FILE"
         echo "$DEPLOY_OUTPUT" >> "../$RESULTS_FILE"
         echo "-----------------------------" >> "../$RESULTS_FILE"
-
         cd ..
     fi
 done
 
 echo "Despliegue completado. Base URLs detectadas:"
-for url in "${BASE_URLS[@]}"; do
-    echo "$url"
+for url in "${!BASE_URLS[@]}"; do
+    echo "BaseURL de ${BASE_URLS[$url]}: $url"
 done
 
 echo "Base URLs detectadas:" >> $RESULTS_FILE
-for url in "${BASE_URLS[@]}"; do
-    echo "$url" >> $RESULTS_FILE
+for url in "${!BASE_URLS[@]}"; do
+    echo "BaseURL de ${BASE_URLS[$url]}: $url" >> $RESULTS_FILE
 done
 
 echo "Los resultados completos del despliegue se han guardado en $RESULTS_FILE."
