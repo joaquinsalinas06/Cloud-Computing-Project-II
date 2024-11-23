bc508a9d70547ec3e34192360901171a02724bed
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-11-23 00:25:26 -0500

    fix: Change dict references

--- scripts/data_setup/utils/uploadFiles.py
+++ scripts/data_setup/utils/uploadFiles.py
@@ -5,9 +5,23 @@ from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 s3 = boto3.client('s3', region_name='us-east-1')
 dynamodb = boto3.client('dynamodb', region_name='us-east-1')
 
-bucket_name = "jss-proyecto2"
+bucket_name = "jss-proyecto2cloud"
 tablas_archivos = {
-    "dev-t_song": "songs.json",
+    "dev-t_album": ["albumsV1.json", "albumsV2.json"],
+    # "dev-t_album": ["albumsV1.json", "albumsV2.json", "albumsData.json"],
+
+    "dev-t_artist": ["artistsV1.json", "artistsV2.json"],
+    # "dev-t_artist": ["artistsV1.json", "artistsV2.json", "artistsData.json"],
+
+    "dev-t_post": ["postsV1.json", "postsV2.json"],
+
+    "dev-t_song": ["songsV1.json", "songsV2.json"],
+    # "dev-t_song": ["songsV1.json", "songsV2.json", "songsData.json"],
+
+    "dev-t_user": ["usersV1.json", "usersV2.json"],
+
+    # "dev-t_comment": ["commentsV1.json", "commentsV2.json"],
+    # "dev-t_playlist": ["playlistsV1.json", "playlistsV2.json"],
 }
 
 
@@ -36,29 +50,26 @@ def batch_write_to_dynamodb(table_name, items):
 
     while 'UnprocessedItems' in response and response['UnprocessedItems']:
         print(f"Reintentando elementos no procesados en la tabla {table_name}...")
-        for table, items in response['UnprocessedItems'].items():
-            print(f"Tabla: {table}")
-            for item in items:
-                print(f"Elemento no procesado: {item}")
         response = dynamodb.batch_write_item(RequestItems=response['UnprocessedItems'])
 
 
 def main():
-    for tabla, archivo_s3 in tablas_archivos.items():
-        print(f"Descargando datos desde {archivo_s3} para la tabla {tabla}...")
+    for tabla, archivos_s3 in tablas_archivos.items():
+        for archivo_s3 in archivos_s3:
+            print(f"Descargando datos desde {archivo_s3} para la tabla {tabla}...")
 
-        try:
-            items = descargar_archivo_s3(bucket_name, archivo_s3)
+            try:
+                items = descargar_archivo_s3(bucket_name, archivo_s3)
 
-            if not isinstance(items, list):
-                print(f"El archivo {archivo_s3} no contiene una lista de JSONs. Saltando...")
-                continue
+                if not isinstance(items, list):
+                    print(f"El archivo {archivo_s3} no contiene una lista de JSONs. Saltando...")
+                    continue
 
-            print(f"Importando datos en la tabla {tabla}...")
-            batch_write_to_dynamodb(tabla, items)
-            print(f"Importación completada para la tabla {tabla}.")
-        except Exception as e:
-            print(f"Error procesando {archivo_s3}: {e}")
+                print(f"Importando datos en la tabla {tabla}...")
+                batch_write_to_dynamodb(tabla, items)
+                print(f"Importación completada para la tabla {tabla} desde {archivo_s3}.")
+            except Exception as e:
+                print(f"Error procesando {archivo_s3}: {e}")
 
 
 if __name__ == "__main__":
