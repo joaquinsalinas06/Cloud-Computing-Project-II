ad72589037b78984db1bffbf4badbe93324f58b1
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-11-27 17:20:10 -0500

    feat: Add Secutity to playlist and make token obtainance of comment

--- backend/Playlist/Handler/addSong.py
+++ backend/Playlist/Handler/addSong.py
@@ -7,12 +7,33 @@ def lambda_handler(event, context):
         provider_id = event['pathParameters'].get('provider_id')
         playlist_id = event['pathParameters'].get('playlist_id')
         song_id = event['pathParameters'].get('song_id')
+        token = event['headers']['Authorization']        
         song_details = json.loads(event['body']).get('song_details')
 
-        if not provider_id or not playlist_id or not song_id or not song_details:
+        if not provider_id or not playlist_id or not song_id or not song_details or not token:
             return {
                 'statusCode': 400,
                 'body': json.dumps({'error': 'Missing parameters'})
+            }
+
+        payload = '{ "token": "' + token +  '" }'        
+        lambda_client = boto3.client('lambda')
+        token_function = os.environ['LAMBDA_FUNCTION_NAME']
+
+        invoke_response = lambda_client.invoke(
+            FunctionName=token_function,
+            InvocationType='RequestResponse',
+            Payload=payload
+        )
+        
+        response_payload = json.loads(invoke_response['Payload'].read())
+        print("Response Payload:", response_payload) 
+        
+        if 'statusCode' not in response_payload or response_payload['statusCode'] != 200:
+            error_message = response_payload.get('body', {}).get('error', 'Unknown error')
+            return {
+                'statusCode': 401,
+                'body': {'error': 'Unauthorized', 'message': error_message}
             }
 
         dynamodb = boto3.resource('dynamodb')
--- backend/Playlist/Handler/deleteSong.py
+++ backend/Playlist/Handler/deleteSong.py
@@ -6,12 +6,33 @@ def lambda_handler(event, context):
     try:
         playlist_id = event['pathParameters'].get('playlist_id')
         song_id = event['pathParameters'].get('song_id')
+        token = event['headers']['Authorization']
 
-        if not playlist_id or not song_id:
+        if not playlist_id or not song_id or not token:
             return {
                 'statusCode': 400,
                 'body': json.dumps({'error': 'Missing parameters'})
             }
+            
+        payload = '{ "token": "' + token +  '" }'        
+        lambda_client = boto3.client('lambda')
+        token_function = os.environ['LAMBDA_FUNCTION_NAME']
+
+        invoke_response = lambda_client.invoke(
+            FunctionName=token_function,
+            InvocationType='RequestResponse',
+            Payload=payload
+        )
+        
+        response_payload = json.loads(invoke_response['Payload'].read())
+        print("Response Payload:", response_payload) 
+        
+        if 'statusCode' not in response_payload or response_payload['statusCode'] != 200:
+            error_message = response_payload.get('body', {}).get('error', 'Unknown error')
+            return {
+                'statusCode': 401,
+                'body': {'error': 'Unauthorized', 'message': error_message}
+            }      
 
         dynamodb = boto3.resource('dynamodb')
         playlist_table = dynamodb.Table(os.getenv('PLAYLIST_TABLE_NAME'))
