5b5ca0fcd2afa89eb8efb1c04f9428bb968739e8
joaquinsalinas06 <joaquin.salinas@utec.edu.pe>
2024-11-27 19:36:27 -0500

    feat: Add getPlaylistByProviderId

---
+++ backend/Playlist/Handler/getPlaylistByUserId.py
@@ -0,0 +1,59 @@
+import boto3
+import os
+import json
+
+def lambda_handler(event, context):
+    try:
+        provider_id = event["pathParameters"].get("provider_id")
+        user_id = event['pathParameters'].get('user_id')
+        token = event['headers']['Authorization']
+
+        if not user_id or not provider_id or not token:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'error': 'Missing user_id parameter or Authorization header'})
+            }
+
+        payload = json.dumps({ "token": token })
+        lambda_client = boto3.client('lambda')
+        token_function = os.environ['LAMBDA_FUNCTION_NAME']
+
+        invoke_response = lambda_client.invoke(
+            FunctionName=token_function,
+            InvocationType='RequestResponse',
+            Payload=payload
+        )
+        
+        response_payload = json.loads(invoke_response['Payload'].read())
+        print("Response Payload:", response_payload)
+
+        if 'statusCode' not in response_payload or response_payload['statusCode'] != 200:
+            error_message = response_payload.get('body', {}).get('error', 'Unknown error')
+            return {
+                'statusCode': 401,
+                'body': json.dumps({'error': 'Unauthorized', 'message': error_message})
+            }
+            
+        dynamodb = boto3.resource('dynamodb')
+        playlist_table = dynamodb.Table(os.getenv('TABLE_NAME'))
+
+        user_index = os.environ['GSI1']
+        
+        response = playlist_table.query(
+            IndexName=user_index,
+            KeyConditionExpression='user_id = :user_id',
+            ExpressionAttributeValues={':user_id': user_id}
+        )
+
+        playlists = response.get('Items', [])
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'playlists': playlists})
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': f"Internal server error: {str(e)}"})
+        }
