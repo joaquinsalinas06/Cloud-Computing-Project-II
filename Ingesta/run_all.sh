efb26836e0912957e37e5fdc9f636a3e215e802d
flauts <hector.nieto@utec.edu.pe>
2024-12-01 15:08:11 -0500

    a

--- Ingesta/run_all.sh
+++ Ingesta/run_all.sh
@@ -11,6 +11,9 @@ carpetas=(
     ["Ingesta6"]="ingesta6"
 )
 
+# Crear el directorio de logs si no existe
+mkdir -p ./logs
+
 # Recorrer todas las carpetas del diccionario
 for carpeta in "${!carpetas[@]}"; do
   imagen="${carpetas[$carpeta]}" # Obtener el nombre de la imagen correspondiente
@@ -23,9 +26,12 @@ for carpeta in "${!carpetas[@]}"; do
   # Construir la imagen Docker
   docker build -t $imagen .
 
-  # Ejecutar el contenedor Docker
+  # Ejecutar el contenedor Docker en segundo plano y redirigir la salida de pantalla a archivo de log
   echo "Corriendo el contenedor para $carpeta con la imagen $imagen..."
-  docker run -d -e CONTAINER_NAME="$carpeta" -v /home/ubuntu/.aws/credentials:/root/.aws/credentials $imagen
+
+  # Usar `docker run` con la opción `-d` para ejecución en segundo plano
+  docker run -d -e CONTAINER_NAME="$carpeta" -v /home/ubuntu/.aws/credentials:/root/.aws/credentials $imagen > "../logs/ingesta${carpeta: -1}.log" 2>&1 &
+  
   # Volver al directorio anterior
   cd ..
 
